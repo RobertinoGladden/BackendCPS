11454d26cc7c700596424737379f005fa475e824
Robertino <gladdenrobertino@gmail.com>
2023-11-27 01:16:41 +0700

    initial commit

--- app.js
+++ app.js
@@ -7,25 +7,38 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+const data = [
+  {
+    id: 1,
+    provinsi: "Banten",
+    kota: "Serang",
+    kecamatan: "Pipitan",
+    desa: "Walantaka"
+  },
+  {
+    id: 2,
+    provinsi: "Jawa Barat",
+    kota: "Bandung",
+    kecamatan: "Dayeuhkolot",
+    desa: "Sukabirus"
+  },
+];
+
 app.get("/api", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      provinsi: "Banten",
-      kota: "Serang",
-      kecamatan: "Pipitan",
-      desa: "Walantaka"
-    },
-    {
-      id: 2,
-      provinsi: "Jawa Barat",
-      kota: "Bandung",
-      kecamatan: "Dayeuhkolot",
-      desa: "Sukabirus"
-    },
-  ]);
+  res.json(data);
+});
+
+app.get("/api/:id", (req, res) => {
+  const id = req.params.id;
+  const foundData = data.find(item => item.id === parseInt(id));
+  
+  if (foundData) {
+    res.json(foundData);
+  } else {
+    res.status(404).json({ message: "Data not found" });
+  }
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-  });
+});
